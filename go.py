68f61e5d7b6d3df9186f2603ab8451e8d0930836
Lee Arnold <lee.arnoldiii@gmail.com>
2020-04-08 11:34:19 -0500

    added python file for basic structures for game

---
+++ go.py
@@ -0,0 +1,18 @@
+'''
+All Python classes will reside in this file
+'''
+# Establish board for game here. This may be subject to change
+class Board:
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+# Establish Stones
+class Stones:
+
+    def __init__(self, board):
+        pass
+
